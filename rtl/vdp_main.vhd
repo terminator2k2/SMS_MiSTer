87a83bd747a6abf5236ec87e9b88f755b0d1d1a3
terminator2k2 <48845336+terminator2k2@users.noreply.github.com>
2026-04-26 08:24:40 +0100

    Update vdp_main.vhd

--- rtl/vdp_main.vhd
+++ rtl/vdp_main.vhd
@@ -219,17 +219,7 @@ begin
 				
 				spr_active := not (spr_color="0000");
 				bg_active  := not (bg_color(3 downto 0)="0000");
-
-		y1 <= '1';
-	if ((x>48 and x<=208) or (ggres='0' and x<=256 and x>0)) and -- thank you slingshot
- 			(mask_column0='0' or x>=9) and display_on='1' then
-			if (((y>=24 and y<168) and smode_M1='0')
-				or ((y>=40 and y<184) and smode_M1='1')
-				or (ggres='0' and y<192) 
-				or (smode_M1='1' and y<224 and ggres='0') 
-				or (smode_M3='1' and y<240 and ggres='0') ) then
-
-				
+					
 				if not spr_active and not bg_active then
 					out_color <= overscan;
 					cram_A <= bg_color(4)&"0000";
